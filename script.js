1699660a2fab58e88fd9342257505d1a9ac07c28
Pradeep <157382496+pradeepdalvi14@users.noreply.github.com>
2026-01-21 22:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('keydown', (e) => {
+    if (e.key >= '0' && e.key <= '9') {
+      code.value = '';
+      setTimeout(() => {
+        if (idx < codes.length - 1) {
+          codes[idx + 1].focus();
+        }
+      }, 10);
+    } else if (e.key === 'Backspace') {
+      setTimeout(() => {
+        if (idx > 0) {
+          codes[idx - 1].focus();
+        }
+      }, 10);
+    }
+  });
+});
 //your JS code here. If required.
